a670377801e26be3a489613a0b97b17f0ea4069b
unknown <MonarkA>
2015-10-27 10:39:45 +0530

    omni-10

--- ListActions.java
+++ ListActions.java
@@ -16,47 +16,12 @@ public class ListActions {
 		}
 		System.out.println("Arraylist add time with 10 elements(in ms) : "+(System.currentTimeMillis() - startTime));
 		
-		arrayList = new ArrayList<Integer>();
-		startTime = System.currentTimeMillis();
-		for (int i = 0; i < 1000; i++) {
-			arrayList.add(i);
-		}
-		System.out.println("Arraylist add time with 1000 elements(in ms) : "+(System.currentTimeMillis()-startTime));
-		
-		arrayList = new ArrayList<Integer>();
-		startTime = System.currentTimeMillis();
-		for (int i = 0; i < 10000; i++) {
-			arrayList.add(i);
-		}
-		System.out.println("Arraylist add time with 10000 elements(in ms) : "+(System.currentTimeMillis()-startTime));
-		
-		startTime = System.currentTimeMillis();
-		arrayList.remove(2);
-		System.out.println("Arraylist removing element(in ms) : "+(System.currentTimeMillis()-startTime));
 		
 		for (int i = 0; i < 10; i++) {
 			linkedList.add(i);
 		}
 		System.out.println("LinkedList add time with 10 elements(in ms) : "+(System.currentTimeMillis() - startTime));
-		
-		linkedList = new LinkedList<Integer>();
-		startTime = System.currentTimeMillis();
-		for (int i = 0; i < 1000; i++) {
-			linkedList.add(i);
-		}
-		System.out.println("LinkedList add time with 1000 elements(in ms) : "+(System.currentTimeMillis()-startTime));
-		
-		linkedList = new LinkedList<Integer>();
-		startTime = System.currentTimeMillis();
-		for (int i = 0; i < 10000; i++) {
-			linkedList.add(i);
-		}
-		System.out.println("LinkedList add time with 10000 elements(in ms) : "+(System.currentTimeMillis()-startTime));
-		
-		startTime = System.currentTimeMillis();
-		linkedList.remove(2);
-		System.out.println("LinkedList removing element(in ms) : "+(System.currentTimeMillis()-startTime));
-		
+			
 	}
 
 }
